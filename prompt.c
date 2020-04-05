fd8dc5612a6139c22222702bc345d46ae6804435
somarae8 <1493@holbertonschool.com>
2020-04-05 00:51:40 +0000

    first commit

---
+++ prompt.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+/**
+ * main - fork & wait example
+ *
+ * Return: Always 0.
+ */
+int main(int arc ,char *env[])
+{
+	char *line = NULL;
+	char *token = NULL, *argv[32];
+	size_t len = 0;
+	ssize_t read;
+	pid_t child_pid;
+	int status, i;
+	
+	arc = arc;
+	while (1)
+	{
+		printf("#cisfun$ ");
+		read = getline(&line, &len, stdin);
+		if (read < 0)
+		{
+			perror("Unable to allocate buffer");
+			return (EXIT_FAILURE);
+		}
+		token = strtok(line, " \t\n\r");
+			for (i = 0; i < 32 && token != NULL; i++)
+			{
+				argv[i] = token;
+				token = strtok(NULL, " \t\n\r");
+			}
+			argv[i + 1] = NULL;
+		if ((child_pid = fork()) == 0)
+		{
+
+			if (execve(argv[0], argv, env) == -1)
+			{
+				perror("Error:");
+			}
+		} else
+		{
+
+			wait(&status);
+		}
+	}
+	free(argv);
+	free(line);
+	exit(EXIT_SUCCESS);
+}
+
+
+
---
+++ shell.h
@@ -0,0 +1,5 @@
+#ifndef SHELL_H
+#define SHELL_H
+int _strcmp(char *s1, char *s2);
+#endif
+
