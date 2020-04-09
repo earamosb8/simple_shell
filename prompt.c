bff1634f57c596806ceaa5e7ba6dd21b9fd7726d
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-04-09 18:16:44 -0500

    new function tokenize running

--- prompt.c
+++ prompt.c
@@ -7,11 +7,11 @@
  */
 int main(int argc, char *env[])
 {
-	char *line = NULL, *token, *argv[32]; /** se cambiaria el *token por **token para la funcion tokenize(); y se eliminaria el argv*/
+	char *line = NULL, **token;
 	size_t len = 0;
 	ssize_t read;
 	pid_t child_pid;
-	int status, i = 0; /** se eliminaria el i = 0, no se necesitaria en la tokenize(); */
+	int status;
 	(void) argc;
 
 	print_sign();
@@ -22,26 +22,15 @@ int main(int argc, char *env[])
 			perror("Unable to allocate buffer");
 			return (EXIT_FAILURE);
 		}
-
-/*		token = tokenize(line); */
-
-		token = strtok(line, " \t\n\r");
-		while (token != NULL)
-		{
-			argv[i] = token;
-			if (_strcmp(argv[0], "exit") == 0)
+		token = tokenize(line);
+		if ((_strcmp(token[0], "exit") == 0)
 			{
 				exit(EXIT_SUCCESS);
 			}
-			token = strtok(NULL, " \t\n\r");
-			i++;
-		}
-		argv[i] = NULL;
-		i = 0;/** hasta aca iria la funcion de tokenizar */
 		child_pid = fork();
 		if ((child_pid) == 0)
 		{
-			if (execve(argv[0], argv, env) == -1) /** aca tendriamos q cambiar el argv[i] por token[i] y argv por token */
+			if (execve(token[0], token, env) == -1)
 			{
 				perror("Error: not valid command");
 				exit(EXIT_FAILURE);
